65caa73d0c0ec0982e569992b4129cc0f4f189b6
Pepa <eles.peter.12@gmail.com>
2020-09-26 21:01:25 +0200

    readme images added

--- src/creator/Main.java
+++ src/creator/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainstage.fxml"));
-        primaryStage.setTitle("SpringButus");
+        primaryStage.setTitle("SpringBootus");
         primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
     }
